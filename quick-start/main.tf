a7585791f5be67f747ec13528391c680854690f8
Wilson Ferreira <willferalneto@gmail.com>
2024-05-23 17:29:59 -0300

    estudos da documentacao oficial hashicorp

---
+++ quick-start/main.tf
@@ -0,0 +1,16 @@
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name  = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "terraform-tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
+
